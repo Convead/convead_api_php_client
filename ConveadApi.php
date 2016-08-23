570870a191d4c8b0126e0b8334616efc4289bf9a
Saveljev Vladimir <v.saveljev@secoint.ru>
2016-08-23 18:22:25 +0300

    Api

---
+++ ConveadApi.php
@@ -0,0 +1,122 @@
+<?php
+
+/*
+  PHP API lib for Convead
+  More information on https://convead.io/api-doc
+*/
+
+class ConveadApi {
+  public $version = '1.0.0';
+
+  public $debug = false;
+  public $host = "tracker.convead.io";
+  public $protocol = "https";
+  public $timeout = 1;
+  public $connect_timeout = 1;
+
+  private $access_token;
+
+  /**
+   * 
+   * @param string $access_token
+   */
+  public function __construct($access_token) {
+    $this->access_token = (string) $access_token;
+  }
+
+  /**
+   * 
+   * @param string $path
+   * @param string $method
+   * @param array $post
+   */
+  public function request($path, $method, $post = array()) {
+    $url = $this->getUrl($path);
+    return $this->send($url, $method, $post);
+  }
+
+  /**
+   * 
+   * @param string $path
+   */
+  private function getUrl($path) {
+    $token = urlencode($this->access_token);
+    return "{$this->protocol}://{$this->host}{$path}?access_token={$token}";
+  }
+
+  /**
+   * 
+   * @param string $url
+   * @param string $method
+   * @param array $post
+   */
+  private function send($url, $method = 'GET', $post = array()) {
+    $this->putLog($url, $method, $post);
+
+    if (!function_exists('curl_exec')) return array('error' => 'No support for CURL');
+
+    if (isset($_COOKIE['convead_track_disable'])) return array('error' => 'Convead tracking disabled');
+
+    $curl = curl_init($url);
+
+    curl_setopt($curl, CURLOPT_TIMEOUT, $this->timeout);
+    curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, $this->connect_timeout);
+    curl_setopt($curl, CURLOPT_FAILONERROR, true);
+    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+
+    if ($method != 'POST') curl_setopt($curl, CURLOPT_CUSTOMREQUEST, $method);
+    
+    if ($post) {
+      if ($method == 'POST') curl_setopt($curl, CURLOPT_POST, 1);
+      curl_setopt($curl, CURLOPT_POSTFIELDS, $this->buildHttpQuery($post));
+
+      curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+    } else {
+      curl_setopt($curl, CURLOPT_POST, false);
+    }
+
+    curl_setopt($curl, CURLOPT_HTTPHEADER, array("Content-Type: application/x-www-form-urlencoded; charset=utf-8", "Accept:application/json, text/javascript, */*; q=0.01"));
+
+    $response = json_decode(curl_exec($curl));
+
+    if (!is_array($response)) $response = array();
+
+    $error = curl_error($curl);
+
+    if ($error) return array('error' => $error);
+    else $response['error'] = false;
+
+    curl_close($curl);
+
+    return $response;
+  }
+
+  /**
+   * 
+   * @param array $query
+   */
+  private function buildHttpQuery($query) {
+    return http_build_query($query);
+  }
+
+  /**
+   * 
+   * @param string $url
+   * @param string $method
+   * @param array $post
+   */
+  private function putLog($url, $method, $post) {
+    if (!$this->debug) return;
+
+    ob_start();
+    print_r($post);
+    $string = ob_get_clean();
+
+    $date = date('Y.m.d H:i:s');
+    $row = "{$date}\n{$method} {$url}\n{$string}\n\n";
+    $filename = dirname(__FILE__) . "/api_debug.log";
+    return file_put_contents($filename, $row, FILE_APPEND);
+  }
+
+}
--- README.md
+++ README.md
@@ -1,64 +1,22 @@
-PHP клиент для отправки евентов в convead
+PHP клиент для отправки запросом в convead API
 -------------------
+
+Подробная информация о API доступна на странице https://convead.io/api-doc
 
 **Пример инициализации библиотеки**
 ```php
-include_once('ConveadTracker.php');
+include_once('ConveadApi.php');
+$access_token = 'ACCESS_TOKEN'; // Токен доступа к API, уникальный для каждого зарегистрированного пользователя Convead
 $app_key      = 'API_KEY'; // APP-ключ вашего аккаунта, можно найти здесь: http://take.ms/Ejv3q
-$host         = $_SERVER['HTTP_HOST']; // Должен совпадать с вашим доменом, указанным в настройках аккаунта
-$visitor_uid  = (is_logged_in() ? $current_user_id : false); // Если юзер авторизован, то подставляется его id, иначе - false
-$guest_uid    = (!empty($_COOKIE['convead_guest_uid']) ? $_COOKIE['convead_guest_uid'] : false);
 
-$visitor_info = array(); // Информация о посетителе
-if ($name) $visitor_info['first_name'] = $name;
-if ($phone) $visitor_info['phone'] = $phone;
-if ($email) $visitor_info['email'] = $email;
-...
-
-$convead = new ConveadTracker($app_key, $host, $guest_uid, $visitor_uid, $visitor_info);
+$api = new ConveadApi($access_token);
 ```
 
-**Пример отправки евента 'update_cart' (передает текущее содержимое корзины)**
+**Пример поиска контакта по email**
 ```php
-$products = $cart->products(); // массив товаров в корзине (массив может быть пустым, если корзина очищена)
+$visitor    = array(
+  'email'=>'mail@example.net'
+);
 
-$items = array();
-foreach ($products as $product) {
-  $items[] = array(
-    'product_id' => $product['product_id'],
-    'qnt' => $product['quantity'],
-    'price' => $product['price']
-  );
-}
-$convead->eventUpdateCart($items);
-```
-
-**Пример отправки евента 'purchase' (передает информацию о совершенной покупке)**
-```php
-$order_id   = $order->id; // id заказа
-$products   = $order->products(); // массив товаров в заказе
-$total_cost = $order->total_cost(); // итоговая стоимость заказа с учетом доставки и скидок
-
-$items = array();
-foreach ($products as $product) {
-  $items[] = array(
-    'product_id' => $product['product_id'],
-    'qnt' => $product['quantity'],
-    'price' => $product['price']
-  );
-}
-$convead->eventOrder($order_id, $total_cost, $items);
-```
-
-**Пример отправки евента 'update_info' (передает только информацию о пользователе)**
-```php
-$convead->eventUpdateInfo();
-```
-
-**Пример отправки кастомного евента**
-```php
-$key        = 'callback'; // Ключ кастомного евента
-$properties = array('phone'=>$phone); // Набор передаваемых данных
-
-$convead->eventCustom($key, $properties);
+$api->request("/api/v1/accounts/{app_key}/visitors", 'GET' $visitor);
 ```
